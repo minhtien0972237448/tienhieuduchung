839a9f934f4e5b880e876cde25031274eda40e24
Nguyen Viet Vu <nguyenvietvu158@gmail.com>
2018-12-30 19:29:57 +0700

    commit code

---
+++ js/app.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    $('.similar_products').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true,
+        navText: ["<img src='images/next_left.png'>", "<img src='images/next_right.png'>"],
+        responsive: {
+            0: {
+                items: 2
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 5
+            }
+        }
+    });
+});
